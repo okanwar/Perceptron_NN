cdd896e65524f85da225b6cc4a4de6ab22763e08
unknown <michael@danafive.com>
2018-10-10 23:05:15 -0700

    Reformated main

--- Main.java
+++ Main.java
@@ -6,52 +6,21 @@ public class Main{
 
 	public static void main(String [] args){
 
-		if(args.length == 0) {
-			//Begin
+		if(args.length == 0){
+			//Run by prompts
 			String mainPrompt = "---- Welcome to the perceptron net ----\n" +
 				"(1) Train a new perceptron net\n" +
 				"(2) Train a net from file\n";
+			
+			//Get mode
+			Scanner input = new Scanner(System.in);
+			System.out.println(mainPrompt);
+			int mode = input.nextInt();
 
-			//			Scanner input = new Scanner(System.in);
-			int userIn = Integer.parseInt(getInput(mainPrompt));
-
-			switch(userIn){
-				case 1:
-					//New perceptron net from scratch			
-					perceptronFromScratch(null);
-					break;
-				case 2:
-					//Load perceptron net from file
-					break;
-				default:
-					break;
-			}
-
-
-		} else {
-			int requiredArguments = 8;
-			//Passed arguments in
-
-			//Validate args
-
-			//Run
-			if(args.length == requiredArguments ){
-				if(args[0].equals("new")){
-					String [] userSettings = new String [7];
-					for(int i = 0; i < 7; i++){
-						userSettings[i] = args[i+1];
-					}
-					perceptronFromScratch(userSettings);
-				}
-			} else {
-				System.out.println(args.length);
-			}
-		}
-	}
-
-	private static void perceptronFromScratch(String [] userSettings){
-		String [] prompts = new String[] {
-			"Enter the training data file name:",
+			if(mode == 1){
+				//New net from scratch
+			String [] prompts = new String[] {
+				"Enter the training data file name:",
 				"--- Initialized weight values ---\n" +
 					"(1) Initialize weights to 0\n" +
 					"(2) Initialize weights to random values between (-0.5,0.5)\n:",
@@ -60,56 +29,27 @@ public class Main{
 				"Enter the learning rate alpha between 0 and 1, not including 0:",
 				"Enter the threshold theta:",
 				"Enter the threshold for measuring weight changes:"
-		};
+			};
 
-		//Get input 
-		if(userSettings == null){
-			userSettings = new String[prompts.length];
-			for(int i = 0; i < prompts.length; i++){
-				userSettings[i] = getInput(prompts[i]);
+			String [] netSettings = getNetSettings(prompts);
+			printNetInitializationSettings(netSettings);
+			} else {
+				//Load from file
 			}
+		} else {
+			//Run with specifications from file
 		}
-
-		printNetInitializationSettings(userSettings);
-	}
-
-	private static void trainPerceptronNet(boolean newNet){
-
-		//Get data file
-		System.out.print("Enter a file name for the training data: ");	
-
-		//Create new net if required
-		if(newNet){
-
-			boolean zeroWeights;
-			int maxEpochs;
-			double learningRate, thresholdTheta, thresholdWeightChanges;
-			String trainingFile, weightFile;
-
-			//Get new net properties
-
-
-
-		}
-
-
-
-		//Open data file
-
-		//Parse data file and created training set
-
-		//Train Perceptron
 	}
 
 	private static void printNetInitializationSettings(String [] userSettings){
 		String [] variables = new String[]{
 			"Training File",
-				"Initialize weights to random values",
-				"Maximum number of training epochs",
-				"Weight settings file",
-				"Learning rate",
-				"Threshold theta",
-				"Threshold to measure weight changes"
+			"Initialize weights to random values",
+			"Maximum number of training epochs",
+			"Weight settings file",
+			"Learning rate",
+			"Threshold theta",
+			"Threshold to measure weight changes"
 		};
 
 		System.out.println("Initializing net with settings:");
@@ -120,14 +60,16 @@ public class Main{
 		System.out.println("------------------------------------");
 	}
 
-	private static String getInput(String prompt){
+	private static String [] getNetSettings(String [] prompts){
+		String [] netSettings = new String[prompts.length];
 		Scanner input = new Scanner(System.in);
-		System.out.print(prompt);
-		String userIn = input.next();
-		System.out.println();
-		input.close();
-		return userIn;
 
-	} 
+		for(int i = 0; i < prompts.length; i++){
+			System.out.print(prompts[i]);
+			netSettings[i] = input.next();
+			System.out.println();
+		}
+		return netSettings;
+	}
 
 }
