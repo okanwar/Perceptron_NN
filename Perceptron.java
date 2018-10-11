2011e6fa0dddf9a59e2fbdb18d3cb0dfa23042c6
Om Kanwar <omkanwar@Oms-MacBook-Pro.local>
2018-10-11 12:38:58 -0700

    train method

--- Perceptron.java
+++ Perceptron.java
@@ -1,11 +1,25 @@
 
-class Perceptron{
-	String dataFile;
-	int maxTrainingEpochs;
-	double learningRate, threshold;
+public class Perceptron{
+   private  PerceptronSettings p_settings;
 
-	public Perceptron(){
-
-	}
-	
+	public Perceptron(PerceptronSettings p_settings){
+	    .p_settings = p_settings;
+   
+    }
+	public static train() {
+        p_settings.getTrainingFile();
+        BufferedReader reader = null;
+        String line = "";
+        try {
+            reader = new BufferedReader (new FileReader (userRunsFile));
+            while ((line = reader.readLine()) != null){
+                StringTokenizer st = new StringTokenizer(line, " ");
+                int index = 0;
+                while(st.hasMoreTokens()) {
+                    st.NextToken();
+                    index++;
+                }
+            }
+        } catch (Exception e) {}
+    }
 }
